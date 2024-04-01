0ae243b2860c277af86f9dd9eeda460c02474697
yeon <test2@test2.com>
2024-04-01 18:35:36 +0900

    [feat/#1] Navbar.tsx 코드 추가

--- src/app/consult/page.tsx
+++ src/app/consult/page.tsx
@@ -1,1 +1,14 @@
 // 실시간 상담 페이지 [최종적으로 나와야하는 = 부위 카테고리, 모든 게시글 리스트, 카테고리별 리스트, 작성하기 버튼]
+// import { AskForm } from "@/components/consult/AskForm";
+import React from "react";
+
+const ConsultPage = () => {
+  return (
+    <div>
+      여긴 ConsultPage
+      {/* <AskForm /> */}
+    </div>
+  );
+};
+
+export default ConsultPage;
