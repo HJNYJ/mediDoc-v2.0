97c4254c7e8312a54311ebbde83127089145bea0
yeon <test2@test2.com>
2024-04-01 17:52:06 +0900

    [feat/#1] - provider.tsx 추가, Navbar 코드 추가

--- src/components/consult/AskForm.tsx
+++ src/components/consult/AskForm.tsx
@@ -1,1 +1,47 @@
 // 질문&답변 Textarea
+
+"use client";
+
+import React from "react";
+
+export const AskForm = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <h1>실시간 상담</h1>
+        </div>
+        <div>
+          <h3>제목</h3>
+          <input
+            type="text"
+            placeholder="예) 이런 증상은 비염인가요?"
+            required
+          />
+        </div>
+        <div>
+          <h3>질문</h3>
+          <textarea
+            placeholder="예) 코가 간지럽고 자꾸 재채기가 나오는데 비염약을 먹어야할까요?"
+            maxLength={500}
+            required
+          />
+        </div>
+        <p>{/** 입력되는 글자수 */} /500</p>
+        <div>
+          <h3>카테고리</h3>
+          <select>
+            <option disabled selected>
+              증상 과목 선택
+            </option>
+            <option>내과</option>
+            <option>외과</option>
+            <option>치과</option>
+            <option>안과</option>
+            <option>이비인후과</option>
+          </select>
+        </div>
+      </section>
+    </>
+  );
+};
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,15 @@
 // 하단 내비게이션 바
+import Link from "next/link";
 import React from "react";
 
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <Link href={"/"}>홈</Link>
+      <Link href={"/login"}>로그인</Link>
+      <Link href={"/mypage"}>마이페이지</Link>
+      <Link href={"/selftest"}>셀프테스트</Link>
+      <Link href={"/consult"}>실시간 상담</Link>
+    </nav>
+  );
 };
