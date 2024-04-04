683213ddf8fa9f12c3b0344e709ca18c46e29b73
Seungchan Hong <hongppa324@gmail.com>
2024-04-04 17:26:23 +0900

    [feat/#9] selftestStore 수정

--- src/shared/zustand/selftestStore.ts
+++ src/shared/zustand/selftestStore.ts
@@ -3,6 +3,8 @@ import { create } from "zustand";
 const useSelftestStore = create((set) => ({
   symptoms: [],
   setSymptoms: (symptoms) => set({ symptoms: symptoms }),
+  selectedPart: "",
+  setSelectedPart: (bodypart) => set({selectedPart: bodypart}),
   selectedSymptoms: [],
   setSelectedSymptoms: (symptoms) => set({selectedSymptoms: symptoms}),
   predictedDiseases: [],
