9feb45e0bbfa24edda5ef5c017c03885a6ba6f8c
Seungchan Hong <hongppa324@gmail.com>
2024-04-04 23:07:16 +0900

    [feat/#11] 로그인 페이지 : 카카오 로그인/로그아웃 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,27 @@
 // 로그인 페이지
+"use client";
+
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import React from "react";
+
+const LoginPage = () => {
+  const supabase = createClientComponentClient();
+
+  const signInWithKakao = async () => {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "kakao",
+      options: { redirectTo: "http://localhost:3000/" }
+    });
+  };
+
+  return (
+    <section className="flex flex-col">
+      <button onClick={() => signInWithKakao()}>카카오로 계속하기</button>
+      <button>네이버로 계속하기</button>
+      <button>Google로 계속하기</button>
+      <button>Apple로 계속하기</button>
+    </section>
+  );
+};
+
+export default LoginPage;
