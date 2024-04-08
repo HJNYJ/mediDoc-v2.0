91b1b8063f8fda91e9dcd301123de5da63a5e458
yeon <test2@test2.com>
2024-04-09 04:56:39 +0900

    [feat] - detail page title, content 추가

--- src/app/consult/[consultId]/page.tsx
+++ src/app/consult/[consultId]/page.tsx
@@ -1,13 +1,30 @@
+"use client";
 // 상담내역 상세페이지[3-2-1. 의사 답변이 달리기 전에 질문자 질문만 있는 세부페이지 ]
-import ConsultItem from "@/components/consult/ConsultItem";
+import { getConsultDetail } from "@/api/supabase";
+// import ConsultItem from "@/components/consult/ConsultItem";
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 
 // const ConsultDetailPage = ({ params }: { params: { id: string } }) => {
 const ConsultDetailPage = ({ params }: { params: { consultId: string } }) => {
+  const {
+    data: consultDetailData,
+    isLoading,
+    error
+  } = useQuery({
+    queryKey: ["consultDetail", params.consultId],
+    queryFn: () => getConsultDetail(params.consultId)
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
       클릭한 질문 : {params.consultId}
-      <ConsultItem />
+      <p>제목: {consultDetailData?.consult_title}</p>
+      <p>내용: {consultDetailData?.consult_content}</p>
+      {/* <ConsultItem /> */}
     </div>
   );
 };
