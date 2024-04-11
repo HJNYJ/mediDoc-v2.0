d2ac03f71ba33fbc550d87a35e15c40e04e500ca
Seungchan Hong <hongppa324@gmail.com>
2024-04-12 01:53:11 +0900

    [Merge] Merge 완료

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,12 +3,6 @@
 
 import { supabase } from "@/api/supabase";
 import React from "react";
-// import { isThereClientSession } from "@/hooks/clientSession";
-
-interface User {
-  email: string;
-  id: string;
-}
 
 const LoginPage = () => {
   const signInWithKakao = async () => {
@@ -18,14 +12,11 @@ const LoginPage = () => {
         options: { redirectTo: "http://localhost:3000/" }
       });
       if (error) throw error;
-
-      const {
-        data: { user }
-      } = await supabase.auth.getUser();
-      if (user) saveUserInfo(user);
     } catch (error) {
-      if (error instanceof Error) console.error(error.message);
-      alert("로그인 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+      if (error instanceof Error) {
+        console.error(error.message);
+        alert("로그인 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+      }
     }
   };
 
@@ -42,28 +33,11 @@ const LoginPage = () => {
         }
       });
       if (error) throw error;
-      const {
-        data: { user }
-      } = await supabase.auth.getUser();
-      if (user) saveUserInfo(user);
     } catch (error) {
-      if (error instanceof Error) console.error(error.message);
-      alert("로그인 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
-    }
-  };
-
-  const saveUserInfo = async (user: User) => {
-    if (!user.email) {
-      console.error("사용자 이메일이 없습니다.");
-      return;
-    }
-    try {
-      const { email, id } = user;
-      await supabase
-        .from("user_info")
-        .upsert([{ user_email: email, user_id: id }]);
-    } catch (error) {
-      if (error instanceof Error) console.error(error.message);
+      if (error instanceof Error) {
+        console.error(error.message);
+        alert("로그인 중 오류가 발생했습니다. 잠시 후 다시 시도해주세요.");
+      }
     }
   };
 
