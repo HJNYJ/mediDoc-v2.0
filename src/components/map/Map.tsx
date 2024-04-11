ea537e1556d8dd367b36819a96cac66cf64ede26
GNooNJo <jjjchoon@nate.com>
2024-04-12 00:28:09 +0900

    [feat/#35] - 병원 위치 카카오 지도 연동 구현

---
+++ src/components/map/Map.tsx
@@ -0,0 +1,54 @@
+import {
+  drawMarkerInfoOnMap,
+  drawMarkerOnMap,
+  setKakaoMap
+} from "@/utils/drawKakaoMap";
+import React, { useEffect } from "react";
+
+declare const kakao: any;
+
+const Map = ({
+  name,
+  latitude,
+  longitude
+}: {
+  name: string;
+  latitude: number;
+  longitude: number;
+}) => {
+  useEffect(() => {
+    const mapScript = document.createElement("script");
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAOMAP_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    mapScript.onload = () => {
+      kakao.maps.load(() => {
+        // 지도 정의
+        const mapDetail = setKakaoMap(latitude, longitude);
+
+        // 지도 그리기
+        const map = new kakao.maps.Map(
+          mapDetail.mapContainer,
+          mapDetail.mapOptions
+        );
+
+        // 마커 그리기
+        const marker = drawMarkerOnMap(map, latitude, longitude);
+
+        // 마커 정보 그리기
+        drawMarkerInfoOnMap(map, marker, name, latitude, longitude);
+      });
+    };
+  }, [name, latitude, longitude]);
+
+  return (
+    <div id="map" className="w-80 h-80">
+      Map
+    </div>
+  );
+};
+
+export default Map;
