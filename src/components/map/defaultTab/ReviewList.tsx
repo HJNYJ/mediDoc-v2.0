f77e83432018618a9a1b2699ccca0e5a8f1163bb
yeon <test2@test2.com>
2024-04-12 14:53:32 +0900

    [feat] - reviewList router

--- src/components/map/defaultTab/ReviewList.tsx
+++ src/components/map/defaultTab/ReviewList.tsx
@@ -2,6 +2,7 @@
 // 방문자 리뷰 section
 
 import useDetailTabStore from "@/shared/zustand/detailTabStore";
+import { useRouter } from "next/navigation";
 
 // 병원 상세 페이지에 밑에 있는 방문자 리뷰 리스트들( 3-4개만 보여짐 ) -> 전체보기로 넘어갈 수 있음
 const ReviewList = () => {
