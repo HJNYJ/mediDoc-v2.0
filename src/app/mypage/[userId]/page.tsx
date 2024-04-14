cc079492a2fc5f98481a926b0af6434b15e0486f
Seungchan Hong <hongppa324@gmail.com>
2024-04-15 02:08:37 +0900

    [feat/#2] 공통 컴포넌트 Tab 완료

--- src/app/mypage/[userId]/page.tsx
+++ src/app/mypage/[userId]/page.tsx
@@ -98,13 +98,13 @@ const MyPage = () => {
 
   return (
     <>
-      <section className="w-[358px] mx-[16px] ">
+      <section className="w-[358px] mx-[16px]">
         <p className="w-[358px] h-[50px] semibold-18 place-content-center">
           마이페이지
         </p>
         {userInfo.map((user) => (
           <div key={user.user_id}>
-            <p className="bold-24">
+            <p className="text-[24px] font-bold px-[16px]">
               {user.user_name}님, <br />
               안녕하세요!
             </p>
