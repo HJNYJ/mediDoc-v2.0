d2c289fee7c0f999ff995fa7f5992b577b01668f
yeon <test2@test2.com>
2024-04-15 06:01:45 +0900

    [feat] - 실시간 상담 폼 구현

--- src/app/consult/[consultId]/page.tsx
+++ src/app/consult/[consultId]/page.tsx
@@ -63,60 +63,62 @@ const ConsultDetailPage = ({ params }: { params: { consultId: string } }) => {
   if (error) return <p>Error: {error.message}</p>;
 
   return (
-    <div className="min-h-screen flex w-[390px] h-[964px]">
-      <div className="shadow-[0_1px_3px_-0px_rgba(0,0,0,0.1)]">
-        <div className="bg-white p-6">
-          <h2 className="semibold-18 text-center mb-5">실시간 상담</h2>
-          <div className="w-[390px]">
-            <p id="user_answer_title" className="semibold-24">
-              {consultDetailData?.consult_title}
-            </p>
-            <p className="regular-13 text-gray-700 mb-7 mt-2">
-              {consultDetailData?.user_name &&
-                `${consultDetailData.user_name.substring(0, 2)}${"*".repeat(Math.max(0, consultDetailData.user_name.length - 2))}`}
-            </p>
-            <p
-              id="user_content_title"
-              className="medium-14 text-gray-800 w-[330px] mb-5"
-            >
-              {consultDetailData?.consult_content}
-            </p>
-            <div className="flex flex-wrap w-[300px] h-[32px]">
-              {consultDetailData?.hashtags
-                ?.toString()
-                .split(",")
-                .map((hashtag: string) => (
-                  <span key={hashtag} className="text-gray-800 regular-12">
-                    <Hashtag key={hashtag} hashtag={hashtag} />
-                  </span>
-                ))}
+    <div className="flex justify-center mt-24">
+      <div className="min-h-screen align-center w-[390px] h-[964px]">
+        <div className="shadow-[0_1px_3px_-0px_rgba(0,0,0,0.1)]">
+          <div className="bg-white p-6">
+            <h2 className="semibold-18 text-center mb-5">실시간 상담</h2>
+            <div className="w-[390px]">
+              <p id="user_answer_title" className="semibold-24">
+                {consultDetailData?.consult_title}
+              </p>
+              <p className="regular-13 text-gray-700 mb-7 mt-2">
+                {consultDetailData?.user_name &&
+                  `${consultDetailData.user_name.substring(0, 2)}${"*".repeat(Math.max(0, consultDetailData.user_name.length - 2))}`}
+              </p>
+              <p
+                id="user_content_title"
+                className="medium-14 text-gray-800 w-[330px] mb-5"
+              >
+                {consultDetailData?.consult_content}
+              </p>
+              <div className="flex flex-wrap w-[300px] h-[32px]">
+                {consultDetailData?.hashtags
+                  ?.toString()
+                  .split(",")
+                  .map((hashtag: string) => (
+                    <span key={hashtag} className="text-gray-800 regular-12">
+                      <Hashtag key={hashtag} hashtag={hashtag} />
+                    </span>
+                  ))}
+              </div>
             </div>
-          </div>
 
-          <div>
-            {userType === "hospital staff" ? (
-              <div>
-                {answerDetailData?.map((item: string) => (
-                  <div key={item}>
-                    <div className="flex flex-col bold-18 text-black mt-5">
-                      {item?.answer}
+            <div>
+              {userType === "hospital staff" ? (
+                <div>
+                  {answerDetailData?.map((item: string) => (
+                    <div key={item}>
+                      <div className="flex flex-col bold-18 text-black mt-5">
+                        {item?.answer}
+                      </div>
+                      <div>{item?.department}</div>
                     </div>
-                    <div>{item?.department}</div>
-                  </div>
-                ))}
-                <ConsultAnswerForm params={params} />
-              </div>
-            ) : (
-              <div>
-                {answerDetailData?.map((item: string, index: number) => (
-                  <div key={item}>
-                    <div>{item?.answer}</div>
-                    <div>{item?.department}</div>
-                  </div>
-                ))}
-              </div>
-            )}
-            <ConsultNotice />
+                  ))}
+                  <ConsultAnswerForm params={params} />
+                </div>
+              ) : (
+                <div>
+                  {answerDetailData?.map((item: string, index: number) => (
+                    <div key={item}>
+                      <div>{item?.answer}</div>
+                      <div>{item?.department}</div>
+                    </div>
+                  ))}
+                </div>
+              )}
+              <ConsultNotice />
+            </div>
           </div>
         </div>
       </div>
