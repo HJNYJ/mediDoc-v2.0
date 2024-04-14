6d222f2050acdedddb498e2a5ef1fdd58e4e2913
jihyun-N <gus2726@naver.com>
2024-04-15 00:40:42 +0900

    [feat/#8] - 메인페이지 - 병원 단건 조회시 해당 DB 정보 출력 구현중 / 메인페이지 구현중

--- src/components/map/review/ReviewItem.tsx
+++ src/components/map/review/ReviewItem.tsx
@@ -2,7 +2,6 @@
 
 import { useRouter } from "next/navigation";
 import ReviewRecent from "./ReviewRecent";
-import ImageTab from "../imageTab/ImageTab";
 
 const ReviewItem = () => {
   const router = useRouter();
@@ -17,7 +16,6 @@ const ReviewItem = () => {
         <h3>방문자 리뷰</h3>
         <button onClick={goToReviewForm}>리뷰 쓰기</button>
       </div>
-      {/* <ImageTab /> */}
       <ReviewRecent />
     </section>
   );
