beda94f8f76e583797a30350f0a8332aa6dc8568
developeryeon <ppont1122@naver.com>
2024-04-17 09:28:02 +0900

    [feat] - 기본 정보 퍼블리싱

--- src/components/map/defaultTab/Notice.tsx
+++ src/components/map/defaultTab/Notice.tsx
@@ -1,11 +1,17 @@
 // 유의사항 div
+import Image from "next/image";
+import notice_btn from "@/assets/icons/notice_btn.png";
 import React from "react";
 
 const Notice = () => {
   return (
     <section>
-      <h2>유의사항</h2>
-      <p>
+      <div className="flex w-[91px] mb-3">
+        <Image src={notice_btn} alt="주의사항" />
+        <h2 className="ml-1 bold-18">유의사항</h2>
+      </div>
+
+      <p className="regular-14 text-gray-800 ml-1">
         위 사이트에서는 추가적인 사항을 제공하지 않습니다.
         <br />
         추가 사항은 해당 병원에 문의 부탁드립니다.
