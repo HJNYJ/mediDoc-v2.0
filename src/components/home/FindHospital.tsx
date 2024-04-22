904ffff7b1076cb84b2399966e4d028e71c0191f
jihyun-N <gus2726@naver.com>
2024-04-22 17:35:45 +0900

    오류수정

--- src/components/home/FindHospital.tsx
+++ src/components/home/FindHospital.tsx
@@ -1,24 +1,18 @@
 "use client";
 
-import { hospitalRegion } from "@/api/supabase";
-import FindHospitalRegionBtn from "./search/FindHospitalRegionBtn";
+import { supabase } from "@/api/supabase";
 import { useQuery } from "@tanstack/react-query";
+import FindHospitalRegionBtn from "./search/FindHospitalRegionBtn";
 
 const FindHospital = () => {
   const { data, isLoading, isError } = useQuery({
     queryKey: ["hospitalRegion"],
-<<<<<<< HEAD
-    queryFn: hospitalRegion
-    // region_id, region_name 둘 다 필요해서 *로 바꿈
-    // -> region_id로 hospitalInfo 테이블안에 region_id가 일치하는 병원들만 뽑아내기 위해
-=======
     queryFn: async () => {
       const response = await supabase.from("hospital_region").select("*");
 
       const { data } = response;
       return data;
     }
->>>>>>> 9c297a4047e0fc505ae9f96eb65728edd4d18aa2
   });
   if (isLoading) {
     <p>로딩 중...</p>;
