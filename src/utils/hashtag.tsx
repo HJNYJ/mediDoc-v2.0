5964d60e180c84eeb84440fb4ceef43095d30084
jihyun-N <gus2726@naver.com>
2024-04-22 17:13:32 +0900

    [feat/#127] - 실시간상담페이지 해시태그 및 답변상태 퍼블리싱(위치 조정) 완료

--- src/utils/hashtag.tsx
+++ src/utils/hashtag.tsx
@@ -5,7 +5,7 @@ interface HashtagProps {
 const Hashtag = ({ hashtag }: HashtagProps) => {
   return (
     <div className="justify-center items-center mr-1 bg-bluegray rounded-3xl mb-2">
-      <span className="text-gray-800 regular-12 px-2 flex-wrap py-[2px]">
+      <span className="text-gray-800 regular-12 flex-wrap relative flex px-2 py-1">
         #{hashtag.replace(/[\[\],_\/'"{}%&*();~`|:?!]/g, "")}
       </span>
     </div>
