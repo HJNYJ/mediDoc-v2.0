da80a3373c8c62d538204264e5ef0190e428480a
yeon <test2@test2.com>
2024-04-24 00:43:33 +0900

    [feat] - config.mjs 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,8 @@ const nextConfig = {
   images: {
     domains: [
       "zxmfaufazajrwupitcjr.supabase.co",
-      "icxxtjvkfjgebycacdab.supabase.co"
+      "icxxtjvkfjgebycacdab.supabase.co",
+      "ifh.cc"
     ]
   }
   // webpack: (config) => {
