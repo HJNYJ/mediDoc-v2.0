8d57740b9185792bc8b276805e51749243a0b91a
yeon <test2@test2.com>
2024-04-24 00:15:12 +0900

    [feat] - next ui로 교체해서 css중

--- src/components/skeleton/ContainerSkeleton.tsx
+++ src/components/skeleton/ContainerSkeleton.tsx
@@ -2,18 +2,18 @@ import { Card, Skeleton } from "@nextui-org/react";
 
 export default function ContainerSkeleton() {
   return (
-    <Card className="w-[200px] space-y-5 p-4" radius="lg">
-      <Skeleton className="rounded-lg">
+    <Card className="w-[200px] space-y-5 p-4 rounded-lg bg-black">
+      <Skeleton className="rounded-lg bg-yellow">
         <div className="h-24 rounded-lg bg-default-300"></div>
       </Skeleton>
-      <div className="space-y-3">
-        <Skeleton className="w-3/5 rounded-lg">
+      <div className="space-y-3 bg-orange">
+        <Skeleton className="w-3/5 rounded-lg bg-slate-600">
           <div className="h-3 w-3/5 rounded-lg bg-default-200"></div>
         </Skeleton>
-        <Skeleton className="w-4/5 rounded-lg">
+        <Skeleton className="w-4/5 rounded-lg bg-gray-900">
           <div className="h-3 w-4/5 rounded-lg bg-default-200"></div>
         </Skeleton>
-        <Skeleton className="w-2/5 rounded-lg">
+        <Skeleton className="w-2/5 rounded-lg bg-white">
           <div className="h-3 w-2/5 rounded-lg bg-default-300"></div>
         </Skeleton>
       </div>
