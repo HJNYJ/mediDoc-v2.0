141150728f393c4bc258c2095e2a5fffc677081c
yeon <test2@test2.com>
2024-04-27 06:30:28 +0900

    [feat] - 무한 스크롤 구현중...

--- src/components/consult/ConsultTabs.tsx
+++ src/components/consult/ConsultTabs.tsx
@@ -1,29 +1,22 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "@/api/supabase";
 import RoundTabs from "../layout/RoundTabs";
-import { useInView } from "react-intersection-observer";
 import type { ConsultType, TabsProps } from "@/types";
 import { throttle } from "lodash";
+import useInterSectionObserver from "./useInterSectionObserver";
 
 const ConsultTabs = ({ setPosts }: TabsProps) => {
-  const { ref: pageEnd, inView } = useInView({
-    threshold: 1
-  });
+  const [itemIndex, setItemIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
 
   // 탭 상태 관리
   const [currentTab, setCurrentTab] = useState("nose");
 
   useEffect(() => {
-    fetchPosts(0, 9);
+    fetchPosts(0, 9); // 초기 게시물을 가져옴
   }, [currentTab]);
 
-  useEffect(() => {
-    if (inView) {
-      fetchMorePosts();
-    }
-  }, [inView]);
-
-  const fetchPosts = async (startRange: number, endRange: number) => {
+  const fetchPosts = async (start: number, end: number) => {
     const { data, error } = await supabase
       .from("consult_info")
       .select(
@@ -38,7 +31,7 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
         `
       )
       .eq("bodyparts", currentTab)
-      .range(startRange, endRange);
+      .range(start, end);
     // .range(0, 9); //파람 값으로 받아오기 useRef 맨밑에 만나면 true, false, 주기
 
     if (error) {
@@ -48,12 +41,31 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
   };
 
   const fetchMorePosts = throttle(async () => {
-    const PAGINATE = 6;
-    const currentPosts = setPosts;
-    const startRange = currentPosts.length;
-    const endRange = startRange + PAGINATE;
-    await fetchPosts(startRange, endRange);
-  }, 1000);
+    setIsLoading(true);
+    await fetchPosts(itemIndex, itemIndex + 5); // 추가 게시물을 가져옴
+    setItemIndex((i) => i + 5);
+    setIsLoading(false);
+  }, 10000);
+
+  const onIntersect: IntersectionObserverCallback = async (
+    [entry],
+    observer
+  ) => {
+    //보통 교차여부만 확인하는 것 같다. 코드는 로딩상태까지 확인함.
+    if (entry.isIntersecting && !isLoading) {
+      observer.unobserve(entry.target);
+      await fetchMorePosts();
+      observer.observe(entry.target);
+    }
+  };
+
+  //현재 대상 및 option을 props로 전달
+  const { setTarget } = useInterSectionObserver({
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+    onIntersect
+  });
 
   const onChangeTabHandler = (tabName: string) => {
     setCurrentTab(tabName);
@@ -101,7 +113,9 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
         />
       </div>
       {/* 스크롤 이벤트 처리 */}
-      <div ref={pageEnd}>{/* Posts */}</div>
+      {/* <div ref={setTarget}>{isLoading && <div>Loading..</div>}</div> */}
+      {/* {isLoading && <div>Loading..</div>} */}
+      <div ref={setTarget}></div>
     </>
   );
 };
