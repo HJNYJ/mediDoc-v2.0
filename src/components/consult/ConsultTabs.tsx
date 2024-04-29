47efdb4716f60420fbba1918618907b869f02b5d
yeon <test2@test2.com>
2024-04-29 09:23:20 +0900

    [feat] - consultPage.tsx 돌려놓기

--- src/components/consult/ConsultTabs.tsx
+++ src/components/consult/ConsultTabs.tsx
@@ -2,21 +2,16 @@ import React, { useEffect, useState } from "react";
 import { supabase } from "@/api/supabase";
 import RoundTabs from "../layout/RoundTabs";
 import type { ConsultType, TabsProps } from "@/types";
-import { throttle } from "lodash";
-import useInterSectionObserver from "./useInterSectionObserver";
 
 const ConsultTabs = ({ setPosts }: TabsProps) => {
-  const [itemIndex, setItemIndex] = useState(0);
-  const [isLoading, setIsLoading] = useState(false);
-
   // 탭 상태 관리
   const [currentTab, setCurrentTab] = useState("nose");
 
   useEffect(() => {
-    fetchPosts(0, 9); // 초기 게시물을 가져옴
+    fetchPosts();
   }, [currentTab]);
 
-  const fetchPosts = async (start: number, end: number) => {
+  const fetchPosts = async () => {
     const { data, error } = await supabase
       .from("consult_info")
       .select(
@@ -30,8 +25,8 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
         consult_photos(*)
         `
       )
-      .eq("bodyparts", currentTab)
-      .range(start, end);
+      .eq("bodyparts", currentTab);
+    //range(0, 5)
     // .range(0, 9); //파람 값으로 받아오기 useRef 맨밑에 만나면 true, false, 주기
 
     if (error) {
@@ -39,33 +34,6 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
     }
     return setPosts(data as ConsultType[]);
   };
-
-  const fetchMorePosts = throttle(async () => {
-    setIsLoading(true);
-    await fetchPosts(itemIndex, itemIndex + 5); // 추가 게시물을 가져옴
-    setItemIndex((i) => i + 5);
-    setIsLoading(false);
-  }, 10000);
-
-  const onIntersect: IntersectionObserverCallback = async (
-    [entry],
-    observer
-  ) => {
-    //보통 교차여부만 확인하는 것 같다. 코드는 로딩상태까지 확인함.
-    if (entry.isIntersecting && !isLoading) {
-      observer.unobserve(entry.target);
-      await fetchMorePosts();
-      observer.observe(entry.target);
-    }
-  };
-
-  //현재 대상 및 option을 props로 전달
-  const { setTarget } = useInterSectionObserver({
-    root: null,
-    rootMargin: "0px",
-    threshold: 0.5,
-    onIntersect
-  });
 
   const onChangeTabHandler = (tabName: string) => {
     setCurrentTab(tabName);
@@ -113,9 +81,7 @@ const ConsultTabs = ({ setPosts }: TabsProps) => {
         />
       </div>
       {/* 스크롤 이벤트 처리 */}
-      {/* <div ref={setTarget}>{isLoading && <div>Loading..</div>}</div> */}
-      {/* {isLoading && <div>Loading..</div>} */}
-      <div ref={setTarget}></div>
+      {/* <div ref={pageEnd}>Posts</div> */}
     </>
   );
 };
